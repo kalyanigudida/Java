725c3344ce57b8bb11a26dbf6cff217205b825ad
kalyanigudida <kalyanigudida5069@gmail.com>
2024-06-19 16:47:49 +0530

    first commit'

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+class Factorial	
+{
+	public static void main(String[]args)
+		{
+			int num=5,start=1,product=1;
+			while(start<=num)
+			{
+			product=product*start;
+				start++;
+			}
+			System.out.println(product);
+		}
+
+}
